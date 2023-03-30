4c8a09213c4ae408c76fc1fce5f4ce7a044be2d1
Rifkhan Jm <rifkhan561@gmail.com>
2023-03-31 00:33:50 +0530

    new_config

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
   cognito: {
-    userPoolId: "ap-south-1_Q0Rv92R3s", // e.g. us-east-2_uXboG5pAb
+    userPoolId: "ap-south-1:861569119044:userpool/ap-south-1_Q0Rv92R3s", // e.g. us-east-2_uXboG5pAb
     userPoolClientId: "5op8lk1c5jp235fr9g98rretl0", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "", // e.g. us-east-2
   },
