10624296564baa9512154c58ded0fcf086c1703e
Rifkhan Jm <rifkhan561@gmail.com>
2023-03-31 00:16:39 +0530

    new_config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-south-1_Q0Rv92R3s", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "5op8lk1c5jp235fr9g98rretl0", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
